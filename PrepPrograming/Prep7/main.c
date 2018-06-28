af83e63130a901087ff5d78ee6cd00b6692495ed
Karinzaa Vitoonkijwanit <minkarin447@gmail.com>
2018-06-28 15:08:03 +0700

    Update Code

--- PrepPrograming/Prep7/main.c
+++ PrepPrograming/Prep7/main.c
@@ -3,5 +3,17 @@
 
 int main()
 {
-    
+   int size[5]= {3, 4, 3, 2, -7};
+    int num;
+    int i,count=0;
+    printf("input number: ");
+     scanf("%d", &num);
+    for(int i = 0; i< 4; i++ ){
+        if(num == size[i]){
+            printf("%d ", i);
+            count++;
+        }
+    }
+    if (count == 0)
+        printf("-1");
 }
