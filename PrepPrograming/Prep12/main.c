65b1497a687e5a56a452ffafd150e821874bcf29
Karinzaa Vitoonkijwanit <minkarin447@gmail.com>
2018-07-03 12:02:06 +0700

    Add Prep12

---
+++ PrepPrograming/Prep12/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void say_42()
+{
+  printf("42 is the answer\n");
+}
+int main()
+{
+  say_42();
+  say_42();
+  say_42();
+  say_42();
+  say_42();
+  say_42();
+  say_42();
+  say_42();
+  say_42();
+  say_42();
+  return 0;
+}
